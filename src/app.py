8a152461036c745ff0c6d85ad4ed5d429e93fbc2
matthewwoo <matthew.edan.woo@gmail.com>
2016-06-17 15:38:08 -0700

    Updated requirements

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_ask import Ask, statement
 
 
 app = Flask(__name__)
-ask = Ask(app, '/')
+ask = Ask(app, '/bot')
 
 @ask.intent('GetRules')
 def rules():
     return statement("Yo")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(port=4990, debug=True)
