e2ae907aa5b805dc436c53574fceb4fb3bfc8411
matthewwoo <matthew.edan.woo@gmail.com>
2016-06-17 16:05:43 -0700

    Added date intent

--- src/app.py
+++ src/app.py
@@ -1,13 +1,25 @@
 from flask import Flask
 from flask_ask import Ask, statement
-
+from src.static.rules import rules
+from datetime import datetime
 
 app = Flask(__name__)
 ask = Ask(app, '/bot')
 
 @ask.intent('GetRules')
-def rules():
-    return statement("Yo")
+def tellrules():
+    return statement(rules)
+
+@ask.intent('GetTrashMan', mapping={'date' : 'Date'})
+def trashquery(date):
+    day = datetime(date).day
+    if 1 <= day <= 10:
+        trash_man = 'Perret'
+    elif 11 <= day <= 19:
+        trash_man= "Woo"
+    else:
+        trash_man = "Zhang"
+    return statement(trash_man)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ src/static/rules.py
@@ -0,0 +1,1 @@
+rules = "Rule One Rule Two Rule Three Rule Four Rule Five"
